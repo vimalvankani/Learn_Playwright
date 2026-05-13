b75eed780dd430394b258c3271a4685fb58eec88
unknown <vimalvankani@gmail.com>
2026-05-14 00:12:17 +0530

    hoistinh_fn.js

---
+++ chapter_04_Javascript_Concept/17_hoisting_fn.js
@@ -0,0 +1,9 @@
+function getUserStatus() {
+    return "active";
+    console.log("status_code");
+    var status_code = "Active";
+    console.log(status_code);
+
+    console.log("vimal vankani");
+}
+getUserStatus();
