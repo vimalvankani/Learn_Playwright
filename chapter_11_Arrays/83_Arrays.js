918a1df3cd9bdaf8e39cf37cbc015874c0e4b0c8
unknown <vimalvankani@gmail.com>
2026-05-29 00:42:46 +0530

    Arrays Example

--- chapter_11_Arrays/83_Arrays.js
+++ chapter_11_Arrays/83_Arrays.js
@@ -1,9 +1,9 @@
-let fruits=[];
-let fruits_fresh = ["apple", "banana", "grape"];
+let fruits=[]; //empty array
+let fruits_fresh = ["apple", "banana", "grape"]; //length 3
 console.log(fruits);
 console.log(fruits_fresh[2]);
 
-let arr =[10, 20, 30, 40, 50];
+let arr =[10, 20, 30, 40, 50]; //length 5
 console.log(arr[0]);
 console.log(arr[1]);
 console.log(arr[2]);
@@ -14,4 +14,4 @@ let testResults = ["pass", "fail", "pass", "skip"];
 let mixed = [1, "Hello", true, null];
 console.log(testResults[0]);
 
-console.log(mixed[3]);
+console.log(mixed[3]); //length 4
--- chapter_11_Arrays/84_Arrays.js
+++ chapter_11_Arrays/84_Arrays.js
@@ -1,24 +1,24 @@
-let browser = ["chrome", "edge", "firefox", "safari"];
+let browser = ["chrome", "edge", "firefox", "safari"]; //length 4
 console.log(browser[0]);
 
-let scores = new Array(3);
+let scores = new Array(3); //here is 3 length of array but no values assigned yet
 scores[0] = 10;
 scores[1] = 20;
 scores[2] = 30;
 
-let scores2 = new Array(1, 2, 3);
+let scores2 = new Array(1, 2, 3); //here we are creating an array with 3 values and length of array is 3.
 
 console.log(scores);
 console.log(scores2);
 
-let numbers = new Array(100, 200, 300, 400, 500);
+let numbers = new Array(100, 200, 300, 400, 500); //length 5
 console.log(numbers);
 
-let test = Array.of(1, 2, 3, 4, 5);
+let test = Array.of(1, 2, 3, 4, 5); //length 5
 console.log(test);
 
-let char = Array.from("Hello");
+let char = Array.from("Hello"); //length 5
 console.log(char);
 
-let numbers2 = Array.from("1234567890");
+let numbers2 = Array.from("1234567890"); //length 10
 console.log(numbers2);
